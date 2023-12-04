cae537f7b15ff32f007f184deb0692f29743f069
Hiroki HIGA <e235733@ie.u-ryukyu.ac.ip>
2023-12-04 15:27:42 +0900

    2nd commit

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,8 @@
-import java.io.IOException;
-
 public class Main {
     public static void main(String[] args){
-        try {
-            String str = null;
-            System.out.println(str.length());
-            
-        } catch (NullPointerException e){
-            System.out.println("NullPointerException:値が設定されていません");
-            System.out.println(e.getMessage());
-        }
+        String str = "壱百満";
+        int value;
+
+        value = Integer.parseInt(str);
     }
 }
